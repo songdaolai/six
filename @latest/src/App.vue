8b5d5bfcb22fd219d5c273aa647ec105fa5d9744
songdaolai <2797257510@qq.com>
2025-03-05 16:51:29 +0800

    改好了哥

---
+++ @latest/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <two/>
+  </template>
+  <script>
+  import two from './components/two.vue';
+  
+  export default {
+    components: {
+      two
+    }
+  };
+  </script>
+  <style>
+  
+  </style>
---
+++ @latest/src/components/two.vue
@@ -0,0 +1,232 @@
+<template>
+      
+    <div class="logo">
+            <div class="feisthang ">
+                <div class="qiche frist1 car ">
+                    <div class="汽车图标">
+                    
+                        <button class="buttn"> 
+                            <i class="fa fa-car"></i>
+                        </button>
+                    </div>
+                    <div class="baoyou ">
+                        <p>平台包邮</p>
+                        <div class="span"><span>极好的</span></div>
+                    </div>
+                </div>
+                <div class="mianfei-text  frist1">
+                    <div class="退货图标">
+                        <button class="一个内部是$的刷新图标 mianfei"> 
+                            <i class="fa fa-refresh "></i>
+                        </button>
+                    </div>
+                    <div class="降价">
+                        <div class="colrs-text ">
+                           <p>免费退货</p>
+                           <div class="span"><span>长达90天</span></div>
+                       </div>
+                    </div>
+                </div>
+                <div class="mianfei-text  frist1">
+                    <div class="获取图标 ">
+                        <button class="手机图标 huoqu"> 
+                            <i class="fa fa-mobile"></i>
+                        </button>
+                    </div>
+                    <div class="获取 ">
+                        <div class="color-text">
+                           <p class="">获取 Temu app</p>
+                       </div>
+                    </div>
+                </div>
+                <div class="xiaosho-s">
+                    <button class="xiaosho">
+                        <p>开始在temu上向数百万买家销售</p>
+                        <button class="jiaru">立即加入</button>
+                    </button> 
+
+                </div>
+            </div>
+            <div class="twohang">
+                <div class="shoppingtupian">
+                    <button class="shopping">
+                        <img src="D:\1\2e3f0aeb0af906f8690ed14b73be43a.png" alt="未加载出来">
+                    </button>
+                </div>
+                <div class="fenlei">
+                    <div class="大拇指">
+                        <div class="大拇指">
+                            
+                            <button><i class="fa-regular fa-thumbs-up"></i></button>
+                        </div>
+                       <div class="畅销商品">
+
+                    </div>
+                    </div>
+                    <div div class="星号">
+                        <div class="星号">
+
+                        </div>
+                        <div class="五星好评">
+
+                        </div>
+                    </div>
+                    <div class="叶子">
+                        <div class="叶子">
+
+                        </div>
+                        <div class="春季大促">
+
+                        </div>
+                    </div>
+                    <div class="车">
+                        <div class="车">
+
+                        </div>
+                        <div class="本地仓库">
+
+                        </div>
+                    </div>
+                    <div class="分类">
+
+                    </div>
+                </div>
+                <div class="sosuokuang">
+                    <div class="搜索框">
+
+                    </div>
+                    <div class="搜索按钮">
+
+                    </div>
+                </div>
+                <div class="yonghuxinxi">
+                    <div class="用户">
+                        <div class="头像">
+
+                        </div>
+                        <div class="登录注册和账户">
+                            <div class="登录/注册">
+
+                            </div>
+                            <div class="账户">
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="客服">
+                        <div class="客服图标">
+
+                        <div class="客服">
+
+                        </div>
+                    </div>
+                    <div class="美利坚">
+                        <div class="国家图标">
+                                
+                        </div>
+                        <div class="zh-hans">
+
+                        </div>
+                    </div>
+                    <div class="购物车">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+
+
+
+</script>
+<style scoped>
+
+.xiaosho{
+    color: #f8df00;
+    background-color: #000000;
+    
+}
+.jiaru{
+    color: #f8df00;
+    background-color: #000000;
+    
+}
+.xiaosho-s{
+    border: none;
+    margin-right: 50px;
+}
+html, body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+.shopping{
+    display: flex;
+    align-items: center;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    border: none;
+}
+.qiche{
+  display: flex;
+}
+.buttn{
+    font-size: 20px;
+    color: #10ff08;
+   
+    background-color: #000000;
+}
+.feisthang{
+    justify-content: space-between;
+    align-items: center;
+    display: flex;
+    border: none; 
+    background-color: #000000;
+    z-index: 1000;
+   width: 100%;
+    position:fixed;
+    top:0;
+    left:0;
+    
+}
+.frist1{
+    margin-left: 100px;
+}
+.baoyou{
+    color: #17f707;
+}
+.mianfei{
+    color: #fdfd02;
+    font-size: 20px;
+    background-color: #000000;
+    border: none;
+}
+.mianfei-text{
+    display: flex;
+}
+.huoqu{
+    color: #eaff00;
+    font-size: 25px;
+    background-color: #000000;
+    border: none;
+}
+.colrs-text{
+    color: #0cfc04;
+}
+.color-text{
+    color: #fffb00;
+}
+.span{
+    font-size: 13px;
+}
+.shoji{
+    font-size: 20px;
+    color: #fefa02;
+}
+.Temu-app{
+    color: #00ff00;
+}   
+</style>
